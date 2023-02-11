f4d9ae70aaf36ad17d60ff44c4dae5575d408bfb
Jesse Gay <jessegay@users.noreply.github.com>
2023-02-10 19:35:34 -0800

    Making progress on Ts

--- js/script.js
+++ js/script.js
@@ -10,7 +10,8 @@ const colorSelect = document.querySelector('#color');
 //const colorOptions =
 //console.log(colorSelect.options);
 //console.log(colorSelect.firstElementChild);
-
+//colorOptionsArray = colorSelect.options;
+//console.log(colorOptionsArray[2].getAttribute('data-theme')); //something is wrong with this approach. colorOptionsArray is an HTMLCollection, but not an array?
 
 
 
@@ -31,6 +32,30 @@ title.addEventListener('change', (e) => {
 
 // disable color select until Design selection is made
 colorSelect.disabled = true;
+// 3.Use the variable for the "Design" or "Theme" menu to listen for the change event on this element.
+designSelect.addEventListener('change', e => {
+  colorSelect.disabled = false;
+  for (let i = 0; i < designSelect.length; i++) {
+    const design = designSelect[i].value;
+    const dataTheme = designSelect[i].getAttribute('data-theme');
+    console.log(design);
+    console.log(dataTheme);
+  }
+} )
+
+//In the event listener, enable the “Color” <select> element that was previously disabled.
+
+//Also in the event listener, loop over the option element children of the "Color" <select> element. The children property will be helpful here. (Why? Can't I use an array manipulation method?)
+
+
+/*In the loop, create variables to reference the following two items:
+The value of the event.target
+The "data-theme" attribute of the loop’s current option element. The getAttribute method will be helpful here. Why can't I use dot notation?
+
+//Log out the two variables that were just created to inspect them. You’ll have to make a selection in the "Design" menu to print those log statements to the console.
+// I think this works? START HERE.
+
+//Still in the loop, create a conditional that checks if the two variables that were just created are equal to one another. If they are, set the hidden property of the loop’s current option element to false, and set the selected attribute of the loop’s current option element to true. And if the two variables are not equal to one another, set the hidden property of the loop’s current option element to true, and set the selected attribute of the loop’s current option element to false.
 
 
 // https://fundamentals.generalassemb.ly/11_unit/dom-cheatsheet.html
